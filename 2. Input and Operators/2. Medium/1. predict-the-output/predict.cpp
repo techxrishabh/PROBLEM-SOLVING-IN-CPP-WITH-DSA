8725f7df80fd7fbf0549a1ad87364995733988a9
techxrishabh <rishabh22204901@gmail.com>
2024-02-24 18:59:45 +0530

    add predict the output

--- 2. Input and Operators/2. Medium/1. predict-the-output/predict.cpp
+++ 2. Input and Operators/2. Medium/1. predict-the-output/predict.cpp
@@ -5,7 +5,8 @@ int main(){
     int q = 2;
     int r = 2;
 
-    cout << (p==q==r); // 0
+    cout << (p==q==r) << "\n" ; // 0
+    cout << (p==(q==r)); // 1  here its under the bracket means it will first do the calculation of bracket 
 
     // first it will check if p == q if it is then it will give true (1) else give false (0)
 
