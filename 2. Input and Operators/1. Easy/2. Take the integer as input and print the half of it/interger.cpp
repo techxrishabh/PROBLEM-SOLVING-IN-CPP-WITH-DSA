04766161757fde21e87138f41f265d87d24a136e
techxrishabh <rishabh22204901@gmail.com>
2024-02-24 20:27:10 +0530

    add Take integer as input and print half of the number

---
+++ 2. Input and Operators/1. Easy/2. Take the integer as input and print the half of it/interger.cpp
@@ -0,0 +1,12 @@
+// Take integer as input and print half of the number
+
+#include <iostream>
+using namespace std;
+int main(){
+    float a;
+    cout << "Print the Integer a : \n";
+    cin >> a;
+    a = int(a/2);
+    cout << "Integer = " << a;
+
+}
