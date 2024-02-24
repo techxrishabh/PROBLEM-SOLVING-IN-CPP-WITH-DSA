c04244e0bf41d60bb0213b3eb1d4deb67fe69df3
techxrishabh <rishabh22204901@gmail.com>
2024-02-24 15:12:28 +0530

    add predict the output

---
+++ 2. Input and Operators/2. Medium/1. predict-the-output/predict.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int p = 1;
+    int q = 2;
+    int r = 2;
+
+    cout << (p==q==r); // 0
+
+    // first it will check if p == q if it is then it will give true (1) else give false (0)
+
+}
