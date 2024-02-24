f31388edc6126efe48cd4a787d59f9c0ee9818c3
techxrishabh <rishabh22204901@gmail.com>
2024-02-24 14:24:39 +0530

    add take input to print the sum

---
+++ 2. Input and Operators/1. Easy/1. sum-of-two-numbers/sum.cpp
@@ -0,0 +1,10 @@
+// Take the Input and print the sum 
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout << "Print the Number a and b : ";
+    cin >> a >> b; 
+    cout << "The sum of a and b is " << a + b;
+}
