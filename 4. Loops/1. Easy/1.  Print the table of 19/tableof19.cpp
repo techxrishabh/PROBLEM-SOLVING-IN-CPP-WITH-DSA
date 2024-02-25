c1960bfdccd320cb89c9314b892f3d60f2f0985b
techxrishabh <rishabh22204901@gmail.com>
2024-02-25 15:48:14 +0530

    add the table of 19

---
+++ 4. Loops/1. Easy/1.  Print the table of 19/tableof19.cpp
@@ -0,0 +1,11 @@
+//  Print the table of 19. 
+
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << 19 << " " << "*" << " " << i << " " << "=" << " " << (19*i) << "\n";
+    }
+    
+}
