347e6e5c4393bf760b209c2e544abbe8fec2444a
techxrishabh <rishabh22204901@gmail.com>
2024-02-28 02:45:14 +0530

    add leap year or not

---
+++ 3. Conditionals/3. Hard/2. Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not. (Considering leap year occurs after every 4 years/leap-year-or-not.cpp
@@ -0,0 +1,20 @@
+// Any year is input through the keyboard. Write a program to determine whether the year is a leap year
+// or not. (Considering leap year occurs after every 4 years
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout << "Enter a Number : ";
+    cin >> x;
+    if (x%4!=0){
+        cout << "Its not a leap year";
+    } else if (x%100){
+        cout << "Its a leap year";
+    } else if(x%400){
+        cout << "not a leap year";
+    } else {
+        cout << "leap year";
+    }
+
+}
