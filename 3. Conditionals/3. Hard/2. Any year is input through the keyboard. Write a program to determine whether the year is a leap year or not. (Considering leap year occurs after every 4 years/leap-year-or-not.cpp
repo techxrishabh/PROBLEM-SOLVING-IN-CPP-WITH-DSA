c4e2cfa1cd1aac08e263af22a8e202fad3ffb291
techxrishabh <rishabh22204901@gmail.com>
2024-02-28 21:51:39 +0530

    modified lap year or not

--- 3. Conditionals/3. Hard/2. Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not. (Considering leap year occurs after every 4 years/leap-year-or-not.cpp
+++ 3. Conditionals/3. Hard/2. Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not. (Considering leap year occurs after every 4 years/leap-year-or-not.cpp
@@ -1,5 +1,5 @@
 // Any year is input through the keyboard. Write a program to determine whether the year is a leap year
-// or not. (Considering leap year occurs after every 4 years
+// or not. 
 
 #include <iostream>
 using namespace std;
