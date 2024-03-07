0275b568e2e597aad05f88f650d76cdd9e160e66
techxrishabh <rishabh22204901@gmail.com>
2024-03-07 22:59:39 +0530

    add predict the output

---
+++ 4. Loops/1. Easy/2. predict the output/predict.cpp
@@ -0,0 +1,35 @@
+// Predict the output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+while ('1' < '2')
+cout << "In while loop" << endl; // will run infinite loop
+
+
+int t = 10;
+while (t /= 2) {
+cout << "Hello" << endl; // will print Hello 3 times
+
+
+for (int x = 1; x * x <= 10; x++)
+cout << "In for loop" << endl; // will print three times In for loop
+
+
+int x = 10, y = 0 ;
+while ( x >= y ) {
+x-- ;
+y++ ;
+cout << x << " " << y << endl ;  
+
+// will print 
+
+// 9    1
+// 8    2
+// 7    3
+// 6    4
+// 5    5
+// 4    6
+
+};
+};
