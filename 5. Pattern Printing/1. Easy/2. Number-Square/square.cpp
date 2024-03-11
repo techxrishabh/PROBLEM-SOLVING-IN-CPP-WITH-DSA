e3be27334386384511c6f0bf80f8969f915e4663
techxrishabh <rishabh22204901@gmail.com>
2024-03-11 20:48:20 +0530

    add number square pattern printing

---
+++ 5. Pattern Printing/1. Easy/2. Number-Square/square.cpp
@@ -0,0 +1,19 @@
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    
+}
