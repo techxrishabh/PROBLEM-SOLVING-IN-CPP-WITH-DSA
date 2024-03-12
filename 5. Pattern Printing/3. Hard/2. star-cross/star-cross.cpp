0af4e0a7f80e5ea139436fc3a90ae62b2c40654a
techxrishabh <rishabh22204901@gmail.com>
2024-03-12 11:40:03 +0530

    add star cross pattern printing

---
+++ 5. Pattern Printing/3. Hard/2. star-cross/star-cross.cpp
@@ -0,0 +1,30 @@
+// *       *
+//  *     * 
+//   *   *  
+//    * *   
+//     *    
+//    * *   
+//   *   *  
+//  *     * 
+// *       *
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 9; i++)
+    {
+        for (int j = 1; j <= 9; j++)
+        {
+            if (i==j || i == i && j == 10-i  )
+            {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+            
+        }
+        cout << "\n";
+    }
+    
+}
