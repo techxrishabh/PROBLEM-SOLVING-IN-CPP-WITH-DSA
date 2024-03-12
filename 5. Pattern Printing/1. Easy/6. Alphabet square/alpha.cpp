d46fbc41c3c6076a2d3d4250845b9b04bde49dbf
techxrishabh <rishabh22204901@gmail.com>
2024-03-12 07:43:02 +0530

    add alphabet square pattern printing

---
+++ 5. Pattern Printing/1. Easy/6. Alphabet square/alpha.cpp
@@ -0,0 +1,19 @@
+// A B C D
+// A B C D
+// A B C D
+// A B C D
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 4; i++)
+    {
+        for (char j = 65; j <= 68; j++)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    
+}
