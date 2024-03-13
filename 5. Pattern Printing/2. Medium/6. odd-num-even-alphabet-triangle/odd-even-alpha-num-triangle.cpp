3bdcd179adb820060c792bce08e0cc03ccd19336
techxrishabh <rishabh22204901@gmail.com>
2024-03-13 09:41:55 +0530

    odd-num-even-alphabet-triangle

---
+++ 5. Pattern Printing/2. Medium/6. odd-num-even-alphabet-triangle/odd-even-alpha-num-triangle.cpp
@@ -0,0 +1,34 @@
+// Input: n = 4
+// Output:
+
+
+// 1
+// A B
+// 1 2 3
+// A B C D
+// 1 2 3 4 5
+
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i%2!=0)
+            {
+                cout << j << " ";
+
+            } else {
+                cout << char(64+j) << " ";
+            }
+            
+        }
+        
+        cout << "\n";
+
+    }
+    
+}
