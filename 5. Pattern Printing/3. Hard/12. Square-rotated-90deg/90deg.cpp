426892e78eb9fcb8cc5d62af4cd0899d70861d44
techxrishabh <rishabh22204901@gmail.com>
2024-04-08 22:00:20 +0530

    added square 90 deg

--- 5. Pattern Printing/3. Hard/12. Square-rotated-90deg/90deg.cpp
+++ 5. Pattern Printing/3. Hard/12. Square-rotated-90deg/90deg.cpp
@@ -11,6 +11,21 @@ using namespace std;
 int main(){
     int nsp = 0;
     int nst = 1;
+
+    for (int i = 1; i <= 1; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 1; k++)
+        {
+            cout << "*";
+        }
+
+    }
+    cout << "\n";
+    
     for (int i = 1; i <= 4; i++)
     {
         for (int j = 1; j <= 3-nsp; j++)
